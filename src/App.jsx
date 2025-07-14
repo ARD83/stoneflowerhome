00dab868ee2252c7996d818732202cf58a78493f
ARD83 <stoneflowerhome@gmail.com>
2025-07-14 23:34:35 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Wifi from "./pages/Wifi";
 import Explore from "./pages/Explore";
+import AddExplore from "./pages/AddExplore";
 import EditExplore from "./pages/EditExplore";
 import GuestGallery from "./pages/GuestGallery";
 import AdminLogin from "./pages/AdminLogin";
@@ -13,23 +14,40 @@ export default function App() {
   return (
     <Router>
       <Navbar />
-      <div className="pt-20"> {/* Add padding for fixed navbar */}
+      <div className="pt-20"> {/* Padding for fixed navbar */}
         <Routes>
+          {/* Public Routes */}
           <Route path="/" element={<Wifi />} />
           <Route path="/wifi" element={<Wifi />} />
           <Route path="/explore" element={<Explore />} />
-          <Route path="/explore/edit/:id" element={
-            <PrivateRoute>
-              <EditExplore />
-            </PrivateRoute>
-          } />
           <Route path="/gallery" element={<GuestGallery />} />
           <Route path="/admin" element={<AdminLogin />} />
-          <Route path="/dashboard" element={
-            <PrivateRoute>
-              <AdminDashboard />
-            </PrivateRoute>
-          } />
+
+          {/* Admin-only Routes */}
+          <Route
+            path="/explore/add"
+            element={
+              <PrivateRoute>
+                <AddExplore />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/explore/edit/:id"
+            element={
+              <PrivateRoute>
+                <EditExplore />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <AdminDashboard />
+              </PrivateRoute>
+            }
+          />
         </Routes>
       </div>
     </Router>
