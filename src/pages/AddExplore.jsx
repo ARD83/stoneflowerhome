08a5ccd9647dda7abffc7c41b360d47876d18f47
ARD83 <stoneflowerhome@gmail.com>
2025-07-19 02:03:26 +0200

    Update AddExplore.jsx

--- src/pages/AddExplore.jsx
+++ src/pages/AddExplore.jsx
@@ -81,7 +81,7 @@ export default function AddExplore() {
 
       <div className="relative z-10 max-w-2xl mx-auto p-4">
         <h1 className="text-4xl font-bold text-white text-center mb-6 drop-shadow">
-          Add Explore Item
+          Add Item
         </h1>
 
         <form
