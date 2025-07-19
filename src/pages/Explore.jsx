bbea3578177cf4537cda50e1b76fed4de5fc3202
ARD83 <stoneflowerhome@gmail.com>
2025-07-19 03:16:47 +0200

    Update Explore.jsx

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -180,7 +180,7 @@ export default function Explore() {
                 onClick={() => navigate("/explore/add")}
                 className="flex items-center gap-2 bg-yellow-200 text-gray-800 px-4 py-2 rounded-full shadow hover:bg-yellow-300 transition"
               >
-                ➕ Add Explore
+                ➕ Add Item
               </button>
             )}
           </div>
