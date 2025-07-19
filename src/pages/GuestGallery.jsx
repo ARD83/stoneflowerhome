cdcb7a69bb6eda94e1e189176d96953b74162796
ARD83 <stoneflowerhome@gmail.com>
2025-07-19 03:31:11 +0200

    Update GuestGallery.jsx

--- src/pages/GuestGallery.jsx
+++ src/pages/GuestGallery.jsx
@@ -76,12 +76,6 @@ export default function GuestGallery() {
               >
                 ➕ Add your Memory
               </button>
-          <Link
-            to="/gallery/add"
-            className="flex items-center gap-2 bg-yellow-200 text-gray-800 px-4 py-2 rounded-full shadow hover:bg-yellow-300 transition"
-          >
-            ➕ Add your Memory
-          </Link>
         </div>
 
         {/* Gallery Cards */}
