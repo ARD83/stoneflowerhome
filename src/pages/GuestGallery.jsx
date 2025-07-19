c06481113c87e6bdc88239e51a63426ed53aeed0
ARD83 <stoneflowerhome@gmail.com>
2025-07-19 03:27:35 +0200

    Update GuestGallery.jsx

--- src/pages/GuestGallery.jsx
+++ src/pages/GuestGallery.jsx
@@ -70,6 +70,12 @@ export default function GuestGallery() {
           <h1 className="text-5xl font-bold drop-shadow-lg">
             Guest Gallery
           </h1>
+          <button
+                onClick={() => navigate("/gallery/add")}
+                className="flex items-center gap-2 bg-yellow-200 text-gray-800 px-4 py-2 rounded-full shadow hover:bg-yellow-300 transition"
+              >
+                ➕ Add your Memory
+              </button>
           <Link
             to="/gallery/add"
             className="flex items-center gap-2 bg-yellow-200 text-gray-800 px-4 py-2 rounded-full shadow hover:bg-yellow-300 transition"
