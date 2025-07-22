ae245aff1e88193bd89c1306defe40de33dcab4a
ARD83 <stoneflowerhome@gmail.com>
2025-07-22 20:11:41 +0200

    Update PrivateRoute.jsx

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,9 +1,18 @@
-
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 export default function PrivateRoute({ children }) {
   const { currentUser } = useAuth();
-  return currentUser ? children : <Navigate to="/admin" />;
+  const ADMIN_EMAIL = "stoneflowerhome@gmail.com";
+
+  // ✅ Check if the user is logged in and is admin
+  if (!currentUser) {
+    return <Navigate to="/admin" replace />;
+  }
+  if (currentUser.email !== ADMIN_EMAIL) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
 }
