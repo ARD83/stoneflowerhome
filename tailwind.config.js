9c3133a00f045c3816c0a94bb815581136a512ed
ARD83 <stoneflowerhome@gmail.com>
2025-07-24 19:52:24 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,12 @@ module.exports = {
         sea: "#879D91",
         olive: "#A3B18A",
         coral: "#D9825B",
-        sunset: "#fd5e53"
+        sunset: "#fd5e53",
+        deep_lagoon: "#3E5F5C",
+        coastal_teal: "#729A94",
+        golden_mirage: "#D1A679",
+        sunkissed_sand: "#E6D5C3",
+        luminous_perl: "#F7F3EC"
       }
     }
   },
